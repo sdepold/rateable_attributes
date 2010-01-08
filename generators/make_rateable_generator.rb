b74d7f849584bc3f583792e15b6650a6e6e148b6
Sascha Depold <sascha@depold.com>
2010-01-08 23:24:44 +0100

    renamed generated migration file

--- generators/make_rateable_generator.rb
+++ generators/make_rateable_generator.rb
@@ -1,7 +1,7 @@
 class MakeRateableGenerator < Rails::Generator::Base
   def manifest
     record do |m|
-      m.migration_template 'migration.rb', 'db/migrate', :migration_file_name => "acts_as_rateable_migration"
+      m.migration_template 'migration.rb', 'db/migrate', :migration_file_name => "make_rateable_migration"
     end
   end
 end
