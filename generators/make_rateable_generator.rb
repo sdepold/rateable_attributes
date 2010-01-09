08ccc72cc8cc86c63a16aa0d637b636fdda4e8aa
Sascha Depold <sascha@depold.com>
2010-01-09 13:47:10 +0100

    added images for visualize method

--- generators/make_rateable_generator.rb
+++ generators/make_rateable_generator.rb
@@ -2,6 +2,9 @@ class MakeRateableGenerator < Rails::Generator::Base
   def manifest
     record do |m|
       m.migration_template 'migration.rb', 'db/migrate', :migration_file_name => "make_rateable_migration"
+      m.directory "public/images/ratings"
+      m.file "star_rated.png", "public/images/ratings/star_rated.png"
+      m.file "star_unrated.png", "public/images/ratings/star_unrated.png"
     end
   end
 end
